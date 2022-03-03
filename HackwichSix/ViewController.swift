53274202f5f2cf833e5b05185496de4beeb1555a
GarciaKG2022 <garciakg@hawaii.edu>
2022-03-03 12:31:25 -0600

    HackwichSix Corrected

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -7,38 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
+{
+    var myFriendsArray = ["Jim", "John", "Jones"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return myFriendsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        
+        let text = myFriendsArray[indexPath.row]
+        
+        cell?.textLabel?.text = text
+        return cell!
+        
+    }
+    
 
 // Part 7
-    var myFriends = ["Jim", "John", "James"]
     
-    override func viewDidLoad() {
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-  
-//Part 8
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
-        let text = myFriendsArray[indexPath.row]
-        cell.textLabel?.text = text
-        return cell
         
     }
-    
-//Part 6
-        
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            
-        return myFriends.count
-        
-        }
     
 }
 
